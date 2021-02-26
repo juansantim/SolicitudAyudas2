5374df332bf933713c3d5af9c47b67687aa81f49
DESKTOP-HORM1N7\valen <juanv.santim@gmail.com>
2021-02-26 13:22:00 -0400

    agregar servidor node para publicar sitio en azure

---
+++ solicitudAyudasClient/server/index.js
@@ -0,0 +1,44 @@
+var express = require('express'), path = require('path'), fs = require('fs');
+
+var compression = require('compression');
+
+var app = express();
+
+var staticRoot = __dirname + '/';
+
+var env = process.env.NODE_ENV || 'development';
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(compression());
+/* other middleware */
+
+/* place any backend routes you have here */    
+
+app.use(function(req, res, next) {
+    //if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    
+    if (accept !== 'html') {
+        return next();
+    }
+
+    if(req.path.includes('package.json')){        
+        next("Invalid Request");
+    }    
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== '') {
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+app.use(express.static(staticRoot));
+
+app.listen(app.get('port'), function() {
+    console.log('app running on port', app.get('port'));
+});
