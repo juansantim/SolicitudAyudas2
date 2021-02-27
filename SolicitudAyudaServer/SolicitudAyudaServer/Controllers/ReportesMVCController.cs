be4aa3f6b4ddd056b4b595bcb3cfdb847511bedb
DESKTOP-HORM1N7\valen <juanv.santim@gmail.com>
2021-02-27 11:14:30 -0400

    cambio nuevamente fastreport para reporteria, ya que rdlc no se pueden generar en linux

--- SolicitudAyudaServer/SolicitudAyudaServer/Controllers/ReportesMVCController.cs
+++ SolicitudAyudaServer/SolicitudAyudaServer/Controllers/ReportesMVCController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FastReport.Data;
 using FastReport.Utils;
 using FastReport.Web;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -13,26 +14,42 @@ namespace SolicitudAyudaServer.Controllers
     public class ReportesMVCController : Controller
     {
         private readonly IConfiguration config;
-        public ReportesMVCController(IConfiguration config)
+        private readonly IHostingEnvironment environment;
+        private string BaseReportUrl;
+
+        public ReportesMVCController(IConfiguration config, IHostingEnvironment environment)
         {
             this.config = config;
+            this.environment = environment;
+            this.BaseReportUrl = $"{environment.ContentRootPath}{System.IO.Path.DirectorySeparatorChar}Reports";
         }
+
+        public string GetReportPath(string reportName)
+        {
+            return $"{BaseReportUrl}{System.IO.Path.DirectorySeparatorChar}{reportName}";
+        }
+
         public IActionResult Index()
         {
             var webReport = new WebReport();
 
             RegisteredObjects.AddConnection(typeof(MsSqlDataConnection));
 
-            MsSqlDataConnection sqlConnection = new MsSqlDataConnection();
-            sqlConnection.Name = "Connection";
+            var reportPath = GetReportPath("ReporteSolicitudes.frx");
+
+            webReport.Report.Load(reportPath);
+
+            MsSqlDataConnection sqlConnection = (MsSqlDataConnection)webReport.Report.Dictionary.Connections[0];            
             sqlConnection.ConnectionString = config.GetConnectionString("adp");
 
-            sqlConnection.CreateAllTables();
-            webReport.Report.Dictionary.Connections.Add(sqlConnection);
-            webReport.Report.Load(@"C:\workspace\adp\SolicitudAyudaServer\SolicitudAyudaServer\Reports\ReporteSolicitudes.frx");
+            webReport.Report.SetParameterValue("SeccionalId", 0);
 
+            webReport.Report.SetParameterValue("Seccional", "Todas");
+            webReport.Report.SetParameterValue("Desde", new DateTime(2021, 1, 1));
+            webReport.Report.SetParameterValue("Hasta", DateTime.Now);
 
             return View(webReport);
         }
     }
 }
+
